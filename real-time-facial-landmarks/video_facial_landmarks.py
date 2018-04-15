38cb56615403f12576beec3d65d380bc010b7fb5
Marcin Flis <marcin.flis@ratel.io>
2018-04-15 21:25:16 +0200

    calculate vertical and horizontall diffs

--- real-time-facial-landmarks/video_facial_landmarks.py
+++ real-time-facial-landmarks/video_facial_landmarks.py
@@ -31,31 +31,68 @@ print("[INFO] camera sensor warming up...")
 vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 time.sleep(2.0)
 
+frame_count = 0
+vert_zero = 0
+horiz_zero = 0
+vert_acc = 0
+horiz_acc = 0
+
 # loop over the frames from the video stream
 while True:
     # grab the frame from the threaded video stream, resize it to
     # have a maximum width of 400 pixels, and convert it to
     # grayscale
     frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+    frame_count += 1
+    # frame = imutils.resize(frame, width=400)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detect faces in the grayscale frame
     rects = detector(gray, 0)
 
-    # loop over the face detections
     for rect in rects:
+
         # determine the facial landmarks for the face region, then
         # convert the facial landmark (x, y)-coordinates to a NumPy
         # array
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
+        leftEyeCorner = shape[39]
+        leftEyebrowCorner = shape[21]
+        noseTop = shape[27]
+
+        vertical_diff = leftEyeCorner[1] - leftEyebrowCorner[1]
+        horizontal_diff = noseTop[0] - leftEyeCorner[0]
+        v_diff = vertical_diff - vert_zero
+        h_diff = horizontal_diff - horiz_zero
+
+        if frame_count < 10:
+            vert_acc += vertical_diff
+            horiz_acc += horizontal_diff
+        else:
+            horiz_zero =  int(horiz_acc / 25)
+            vert_zero = int(vert_acc / 25)
+
         # loop over the (x, y)-coordinates for the facial landmarks
         # and draw them on the image
         for (x, y) in shape:
             cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
+        cv2.putText(frame, "vertical diff: {}".format(vertical_diff), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "horiz diff {}".format(horizontal_diff), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        cv2.putText(frame, "vert zero: {}".format(vert_zero), (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "horiz zero {}".format(horiz_zero), (300, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        cv2.putText(frame, "v_diff: {}".format(v_diff), (10, 90),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "h_diff {}".format(h_diff), (300, 90),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     # show the frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
